80995bf84708955c8affeea5cd47725a08610fd7
Your Name <you@example.com>
2019-09-19 21:29:51 -0300

    cuarto commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+//  const mongoose = require ('mongoose');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req,res)=> {
+res.send("hola");
+   });
+app.listen(port);
+
